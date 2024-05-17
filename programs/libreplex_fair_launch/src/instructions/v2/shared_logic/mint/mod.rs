fb4189214f259427ec18678f356dcc97541c3f53
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2024-05-17 23:07:37 +0100

    Move libreplex_fair_launch to separate repo for the purposes of audit

---
+++ programs/libreplex_fair_launch/src/instructions/v2/shared_logic/mint/mod.rs
@@ -0,0 +1,10 @@
+
+pub mod mint_token_2022_logic;
+pub use mint_token_2022_logic::*;
+
+pub mod mint_all_fungibles_2022_logic;
+pub use mint_all_fungibles_2022_logic::*;
+
+
+pub mod update_deployment_and_hashlist;
+pub use update_deployment_and_hashlist::*;
---
+++ programs/libreplex_fair_launch/src/instructions/v2/shared_logic/mod.rs
@@ -0,0 +1,12 @@
+
+pub mod inscriptions;
+pub use inscriptions::*;
+
+pub mod deploy;
+pub use deploy::*;
+
+pub mod swaps;
+pub use swaps::*;
+
+pub mod mint;
+pub use mint::*;
